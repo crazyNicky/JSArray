1b59a4fbe3704240a2b9d0293f0082774277bb07
Nikolay Shtanin <nikolay.shtanin@gmail.com>
2023-05-03 12:55:43 +0200

    working Version

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 
-let Massiv = [];
-for (let i=0;i<10;i++)Massiv.push(i);
-Massiv.forEach(item =>{
-    document.getElementById("output").innerHTML += "<li>"+item+"</li>"});
+const myArray = [];
+
+function addToArray() {
+  const inputField = document.getElementById("input");
+  const inputValue = inputField.value;
+
+  myArray.push(inputValue);
+  inputField.value = "";
+  displayArray();
+}
+
+function displayArray() {
+  const output = document.getElementById("output");
+  output.innerHTML = "";
+  myArray.forEach(item =>{
+    output.innerHTML += "<li>"+item+"</li>"});
+}
